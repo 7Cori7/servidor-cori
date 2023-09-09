1baa4c6fdcd1089d0ff8c3bba2e1a3245956029f
7Cori7 <corinaidldesign@gmail.com>
2023-09-09 09:24:06 -0400

    actualize el proyecto

--- PROYECTO_ Servidor/home/index.js
+++ PROYECTO_ Servidor/home/index.js
@@ -1,88 +1,93 @@
-//* SELECTORES
-
-const formC = document.querySelector('#form-create');
+//* SELECTORES:
 const formL = document.querySelector('#form-login');
-
-const createInput = document.querySelector('#create-input');
-const loginInput = document.querySelector('#login-input');
-
+const formC = document.querySelector('#form-create');
+const inputL = document.querySelector('#login-input');
+const inputC = document.querySelector('#create-input');
 const notificacion = document.querySelector('.notification');
+
 
 //* EVENTOS:
 
-//Para crear el usuario:
-
-formC.addEventListener('submit', async e=>{
+formC.addEventListener('submit', async e=>{ //? <--- operacion asincrona
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/users', {metod: 'GET'});
-    const users = await response.json();
 
-    //voy a buscar el usuario que estoy colocando en el campo dentro del recurso users:
-    const user = users.find(user=> user.username === createInput.value);
+    const respuesta = await fetch('http://localhost:3000/user', {
+        method:'GET'
+    });
 
-    //validamos:
+    const users = await respuesta.json();
 
-    if(!createInput.value){//<---si el campo esta vacio
-        
-        notificacion.innerHTML = "El campo de usuario no puede estar vacio";
+    //buscar si ya existe un usuario con el mismo nombre:
+    const user = users.find(user=>user.nombre === inputC.value);
+    ////console.log(user)
+
+    //validaciones:
+    if(!inputC.value){
+        //el campo esta vacio
+        console.log('El usuario no puede estar vacio')
+        notificacion.innerHTML = 'El usuario no puede estar vacio';
         notificacion.classList.add('show-notification');
 
         setTimeout(()=>{
-            notificacion.classList.remove('show-notification')
+            notificacion.classList.remove('show-notification');//<--esta clase le hace display a la notificacion
         },2500);
-    }else if(user){//<---caso de que el usuario ya existe
-
-        notificacion.innerHTML = "El usuario ya existe";
+    }else if(user){
+        //ya existe el usuario
+        ////console.log('ya existe')
+        notificacion.innerHTML = 'El usuario ya existe';
         notificacion.classList.add('show-notification');
 
         setTimeout(()=>{
-            notificacion.classList.remove('show-notification')
+            notificacion.classList.remove('show-notification');
         },2500);
     }else{
-
-        //En caso de crear un usuario nuevo:
-        await fetch('http://localhost:3000/users',{
-            metod: 'POST',
-            headers:{
-                'Content-type':'aplication/json'
+        //Crear el usuario
+        ////console.log('creando nuevo usuario')
+        await fetch('http://localhost:3000/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
             },
-            body: JSON.stringify({username: createInput.value})    
-        });
+            body:JSON.stringify({nombre:inputC.value})//<--solo puede recibir strings
+        })
 
-        notificacion.innerHTML = `El usuario ${createInput.value} ha sido creado`;
+
+        notificacion.innerHTML = `El usuario ${inputC.value} ha sido creado`;
         notificacion.classList.add('show-notification');
 
         setTimeout(()=>{
             notificacion.classList.remove('show-notification');
         },2500);
 
-        createInput.value = '';
+        inputC.value = '';
     }
-
-})
-
-//Para hacer login:
+});
 
 formL.addEventListener('submit', async e=>{
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/users', {metod: 'GET'});
-    const users = await response.json();
+    const respuesta = await fetch('http://localhost:3000/user', {
+        method: 'GET'
+    });
 
-    //Buscar el usuario:
-    const user = users.find(user=> user.username === loginInput.value);
+    const users = await respuesta.json();
 
-    //Validar:
+    const user = users.find(user=>user.nombre === inputL.value);
+
+    //validar:
     if(!user){
-        notificacion.innerHTML = "El usuario no existe";
+        //si no existe
+        notificacion.innerHTML = 'El usuario no existe';
         notificacion.classList.add('show-notification');
-
         setTimeout(()=>{
             notificacion.classList.remove('show-notification');
-        },2500);
+        }, 2500);
     }else{
-        //Si se hace el login, primero se guarda en el localstorage y luego se abre una nueva ventana(pagina):
-        localStorage.setItem('user', JSON.stringify(user));
+        //si existe, debe tomar el valor y guardarlo en el localstorage
+        localStorage.setItem('user', JSON.stringify(user))
         window.location.href = '../tareas/tareas.html';
     }
+
 })
+
+//* FUNCIONES:
